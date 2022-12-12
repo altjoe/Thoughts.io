782768f450a2e12744c4bb584a21a8221b2606b5
JoeMac <meyer.joer5@gmail.com>
2022-12-12 16:52:33 -0500

    added new branch to create better connection to ssh and database

---
+++ crypto/sentiment-server/api/bitcoin/old/dailycoin.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import urllib.request, sys, time
+import pandas as pd
+import requests
+from db import database as db_manager
+import time
+import datetime
+# this wiill be a bitcoin specific sentiment
+
+urls = ['https://cryptodaily.co.uk/tag/bitcoinrobot/', 'https://newsbtc.com/news/bitcoin/']
+
+def getpagecontents(pageurl):
+    page = requests.get(pageurl)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    paragraphs = soup.find_all('p')
+    pagetext = ''
+    for paragraph in paragraphs:
+        spans = paragraph.find_all('span')
+        for span in spans:
+            pagetext += span.text.strip() + ' '
+    time.sleep(1)
+    return pagetext
+
+def historical_dailycoin(db : db_manager) :
+    for pagenum in range(1000):
+        url = 'https://dailycoin.com/bitcoin/?page={pagenum}'
+        page = requests.get(url)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.text, 'html.parser')
+            article_cards = soup.find_all('div', attrs={'class' : 'mkd-post-item-inner'})
+            for article in article_cards:
+                titlelink = article.find('a', attrs={'class' : 'mkd-pt-title-link'})
+                title = titlelink.text.strip().replace('\n', '')
+                link = titlelink['href'].strip()
+                author = article.find('span', attrs={'class' : 'post-info-author'}).text.rstrip().replace('by ', '').replace('\n', '').lstrip(' ')
+                date = article.find('span', attrs={'class' : 'date'}).text.rstrip()
+                entry_time = article.find('span', attrs={'class' : 'time'}).text.rstrip()
+                articlecontents = getpagecontents(link)
+                articledata = pd.DataFrame(data={'title': title, 'author' : author, 'date' : date, 'time' : entry_time, 'contents' : articlecontents, 'link' : link}, index=[0])
+
+                # db.df_to_table(articledata, 'articles', 'title')
+                db.insert_df(articledata, 'articles')
+                print(title, '- Inserted into table')
+        else:
+            print(f'[{datetime.datetiem.now()}] All historical data collected for Daily Coin waiting for new data.')
+            break
+        print(f'Page: {pagenum}')
+
+def automated_collection():
+    url = 'https://dailycoin.com/bitcoin/'
+    while True:
+        pass
+
+def main():
+    db = db_manager()
+    with open('clean_conn.sql') as file:
+        print(file.read())
+    db.run_file('clean_conn.sql')
+    historical_dailycoin(db)
+    
+    # print(db.get_all_table_info())
+    # db.query('select * from schema')
+
+    # time.sleep(5)
+
+
+
+
+        
+
+
+
+main()
---
+++ crypto/sentiment-server/api/bitcoin/old/tunnel.py
@@ -0,0 +1,18 @@
+from sshtunnel import SSHTunnelForwarder
+import datetime 
+import time
+
+def open_tunnel():
+    while True:
+        with SSHTunnelForwarder(('az1-ts100.a2hosting.com', 7822),
+                                ssh_password="04x.IN)Mp6du1D",
+                                ssh_username="alterejo",
+                                remote_bind_address=('localhost', 5432),
+                                local_bind_address=('localhost', 5432)) as tunnel:
+            while True:
+                print(f'Connection open {datetime.datetime.now()}')
+                time.sleep(5)
+        print(f'Connection reopening in 5 seconds {datetime.datetime.now()}')
+        time.sleep(5)
+# open_tunnel()
+open_tunnel()
